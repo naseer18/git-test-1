75201b27c4172616ce5cfa70b2631de1e80bb06c
naseer <naseerspa@gmail.com>
2025-05-24 20:21:22 +0530

    2nd commit

---
+++ ex2-dfuntion.py
@@ -0,0 +1,9 @@
+def double(n):
+    return 2 * n #returns double the value of given number
+print("To call this fucntion using its name and assing the value of 
+the function to a variable, we can do the following:")
+#x  = double(5) #assigns the value of the function to a variable
+#print(x)
+print ("the program ran sucessfully")
+#........................................
+print("fist modifcation for 2nd commit")
