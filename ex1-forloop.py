7f3067f8247bd7356336c929afc642062c468ce9
naseer <naseerspa@gmail.com>
2025-05-25 17:29:49 +0530

    modified a file ex1 from devteam1 for first time

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -1,11 +1,10 @@
+# File: ex1-forloop.py
 def double(n):
-    return 2 * n #returns double the value of given number
-print("To call this fucntion using its name and assing the value of 
-the function to a variable, we can do the following:")
-#x  = double(5) #assigns the value of the function to a variable
-#print(x)
-print ("the program ran sucessfully")
-#........................................
-print("fist modifcation for 2nd commit")
-#........................................
-print("added this line from shaik user from terminal")
+    return 2 * n  # returns double the value of given number
+
+print("To call this function using its name and assign the value of the function to a variable, we can do the following:")
+x = double(5)  # assigns the value of the function to a variable
+print(x)
+print("the program ran successfully")
+#......................
+print("added this line from devteam1 fro firsttime")
