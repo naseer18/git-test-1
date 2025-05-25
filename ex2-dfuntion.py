f138fc51365506eaf7900c479b66aae566f6e920
naseer <naseerspa@gmail.com>
2025-05-25 17:54:38 +0530

    fixed errors'

--- ex2-dfuntion.py
+++ ex2-dfuntion.py
@@ -1,12 +1,13 @@
+# File: ex2-function.py
 def double(n):
-    return 2 * n #returns double the value of given number
-print("To call this fucntion using its name and assing the value of 
-the function to a variable, we can do the following:")
-#x  = double(5) #assigns the value of the function to a variable
-#print(x)
-print ("the program ran sucessfully")
+    return 2 * n  # returns double the value of given number
+print("To call this function using its name and assign the value of the function to a variable, we can do the following:")
+x = double(5)  # assigns the value of the function to a variable
+print(x)
+print("the program ran successfully")
+
 #........................................
-print("fist modifcation for 2nd commit")
+print("fist modification for 2nd commit")
 #........................................
 print("added this line from github for the first time")
 #........................................
